2cc2cb109670f6421db6dd7b0150c38cfbfa159d
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:49:35 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "mysql_flexible_server_firewall_rules" {
+  description = "All mysql_flexible_server_firewall_rule resources"
+  value       = azurerm_mysql_flexible_server_firewall_rule.mysql_flexible_server_firewall_rules
+}
+
